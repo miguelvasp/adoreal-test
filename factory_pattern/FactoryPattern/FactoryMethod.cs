29e747e35e36b73ee7c3ce4b73d9ea672552f42d
Miguel Parra <miguelv.asp@gmail.com>
2026-01-13 15:16:11 -0300

    feat(patterns): add generic Factory Method implementation (C#)
    - implement Creator<T> with overridable Create() factory method
    - provide DelegateCreator<T> and DelegateCreator<T,TArg> for flexible object creation
    - add console demo showcasing usage with different types

---
+++ factory_pattern/FactoryPattern/FactoryMethod.cs
@@ -0,0 +1,37 @@
+﻿namespace Patterns.FactoryMethod
+{
+    public abstract class Creator<T>
+    {
+        protected abstract T Create();
+
+        public virtual T Operation()
+        {
+            var instance = Create();
+            return instance;
+        }
+    }
+
+    public sealed class DelegateCreator<T> : Creator<T>
+    {
+        private readonly Func<T> _factory;
+
+        public DelegateCreator(Func<T> factory)
+            => _factory = factory ?? throw new ArgumentNullException(nameof(factory));
+
+        protected override T Create() => _factory();
+    }
+
+    public sealed class DelegateCreator<T, TArg> : Creator<T>
+    {
+        private readonly Func<TArg, T> _factory;
+        private readonly TArg _arg;
+
+        public DelegateCreator(Func<TArg, T> factory, TArg arg)
+        {
+            _factory = factory ?? throw new ArgumentNullException(nameof(factory));
+            _arg = arg;
+        }
+
+        protected override T Create() => _factory(_arg);
+    }
+}
---
+++ factory_pattern/FactoryPattern/Program.cs
@@ -0,0 +1,33 @@
+﻿using Patterns.FactoryMethod;
+
+
+internal static class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("Factory Method (generic) demo\n");
+
+        //Simple sample using Guid
+        var guidCreator = new DelegateCreator<Guid>(() => Guid.NewGuid());
+        var id1 = guidCreator.Operation();
+
+        Console.WriteLine($"Guid #1: {id1}");
+
+
+        // HttpClient
+        var httpClientCreator = new DelegateCreator<HttpClient>(() =>
+        {
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri("https://www.adoreal.com/en"),
+                Timeout = TimeSpan.FromSeconds(10)
+            };
+            client.DefaultRequestHeaders.UserAgent.ParseAdd("FactoryMethodDemo/1.0");
+            return client;
+        });
+
+        using var clientInstance = httpClientCreator.Operation();
+        Console.WriteLine($"HttpClient BaseAddress: {clientInstance.BaseAddress}");
+        Console.WriteLine($"HttpClient Timeout: {clientInstance.Timeout}\n");
+    }
+}
